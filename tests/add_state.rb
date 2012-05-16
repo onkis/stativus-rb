c9213b12bbc4a72ce7bca3f2bf507e0de65253f0
Mike Ball <mike.ball3@gmail.com>
2012-05-16 12:26:27 -0400

    more passing tests

--- tests/add_state.rb
+++ tests/add_state.rb
@@ -1,7 +1,7 @@
 require 'test/unit'
 require Dir.pwd()+'/stativus.rb'
 require  Dir.pwd()+'/tests/test_states'
-
+require 'ruby-debug'
 class AddState < Test::Unit::TestCase
   def setup
     @statechart = Stativus::Statechart.new
@@ -10,13 +10,17 @@ class AddState < Test::Unit::TestCase
     @statechart.add_state(B)
     @statechart.add_state(C)
 
-    @statechart.goto_state("B", "default")
-    @statechart.goto_state("C", "default")
+    @statechart.goto_state("B", Stativus::DEFAULT_TREE)
+    @statechart.goto_state("C", Stativus::DEFAULT_TREE)
   end #end setup
   
   
   def test_states_were_added
-    assert(true, "did work")
+    assert(true, "Sanity check")
+    assert_equal(1, @statechart.all_states.keys.length, "should have one tree")
+    assert_equal(3, @statechart.all_states[Stativus::DEFAULT_TREE].keys.length, 
+                  "should have 3 states in default tree")
+    
     
   end
 
